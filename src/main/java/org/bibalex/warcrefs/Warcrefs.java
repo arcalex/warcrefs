63d73d4011d008402782d6e464161714561f0a60
msm <msm@10.8.36.13>
2015-03-22 16:10:01 +0200

    Handling illegal argument exception if user wrongly set bufferSize.

--- src/main/java/org/bibalex/warcrefs/Warcrefs.java
+++ src/main/java/org/bibalex/warcrefs/Warcrefs.java
@@ -9,16 +9,24 @@ import java.io.IOException;
 
 public class Warcrefs 
 {
-    public static void main (String[] args) throws IOException
+    public static void main( String[] args ) throws IOException
     {
-        int bufferSize = 8129;                      // args[0]
-        String digestsPath = "/home/msm/digests";   // args[1]
-        String rootDirPath = "/home/msm";           // args[2]
+        int bufferSize = 8129;                               // args[0]
+        String digestsPath = "/home/msm/warcrefs/digests";   // args[1]
+        String rootDirPath = "/home/msm/warcrefs";           // args[2]
         
-        Deduplicator deduplicator = new Deduplicator(bufferSize);
-
-        // Command the deduplicator to deduplicate WARC files under the
-        // directory at args[2] using the digests in file args[1].
-        deduplicator.deduplicate(digestsPath, rootDirPath);
+        try
+        {
+            Deduplicator deduplicator = new Deduplicator( bufferSize );
+            
+            // Command the deduplicator to deduplicate WARC files under the
+            // directory at args[2] using the digests in file args[1].
+            // TODO: should handle IOException and FileNotFound Exception
+            deduplicator.deduplicate( digestsPath, rootDirPath );
+        }
+        catch( IllegalArgumentException e )
+        {
+            System.out.println( e.getMessage() );
+        }
     }
 }
